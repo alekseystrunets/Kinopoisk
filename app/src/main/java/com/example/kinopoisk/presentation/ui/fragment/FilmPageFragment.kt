78209f6b66aba267304ff08b0cfb7b267b771770
ALeksey Strunets <a.strunetss@gmail.com>
2025-02-16 16:00:20 +0300

    Creating a page for watching a movie and its information

---
+++ app/src/main/java/com/example/kinopoisk/presentation/ui/fragment/FilmPageFragment.kt
@@ -0,0 +1,26 @@
+package com.example.kinopoisk.presentation.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kinopoisk.R
+
+class FilmPageFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_film_page, container, false)
+    }
+
+    }
+}
